e8136c220650e42fb4c2abda523bd0259780ddf0
Cranky <marco_reko@hotmail.de>
2025-12-11 14:14:35 +0100

    Align mekanism hut blockstate and lang

--- src/main/java/com/allthecolonists/block/custom/MekanismWorkerHutBlock.java
+++ src/main/java/com/allthecolonists/block/custom/MekanismWorkerHutBlock.java
@@ -1,11 +1,26 @@
 package com.crankgpt.allthecolonists.block.custom;
 
-import net.minecraft.world.level.block.Block;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.MapColor;
 
-public class MekanismWorkerHutBlock extends Block {
+public class MekanismWorkerHutBlock extends HorizontalDirectionalBlock {
     public MekanismWorkerHutBlock() {
         super(BlockBehaviour.Properties.of().mapColor(MapColor.METAL));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 }
--- src/main/java/com/allthecolonists/registry/ModBlocks.java
+++ src/main/java/com/allthecolonists/registry/ModBlocks.java
@@ -1,12 +1,10 @@
 package com.allthecolonists.registry;
 
 import com.allthecolonists.AllTheColonists;
+import com.crankgpt.allthecolonists.block.custom.MekanismWorkerHutBlock;
 
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -23,11 +21,9 @@ public class ModBlocks {
 
     // Mekanism Worker Hut Block
     public static final DeferredBlock<Block> MEKANISM_WORKER_HUT =
-            BLOCKS.registerSimpleBlock(
+            BLOCKS.register(
                     "mekanism_worker_hut",
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.METAL)
-                            .strength(2.0f)
+                    () -> new MekanismWorkerHutBlock()
             );
 
     public static final DeferredItem<BlockItem> MEKANISM_WORKER_HUT_ITEM =
