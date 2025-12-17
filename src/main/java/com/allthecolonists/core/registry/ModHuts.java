0db2b33ac3032efdb58e6f39794319afc5dc07a8
Cranky <marco_reko@hotmail.de>
2025-12-17 19:40:35 +0100

    Remove unfinished Mekanism hut stubs

--- src/main/java/com/allthecolonists/core/registry/ModHuts.java
+++ src/main/java/com/allthecolonists/core/registry/ModHuts.java
@@ -1,13 +1,9 @@
 package com.allthecolonists.core.registry;
 
 import com.allthecolonists.core.AllTheColonists;
-import com.allthecolonists.core.colony.buildings.workerbuildings.BuildingMekanism;
-import com.allthecolonists.core.blocks.custom.MekanismWorkerHutBlock;
 import com.minecolonies.api.colony.buildings.registry.BuildingEntry;
 import com.minecolonies.api.colony.buildings.registry.IBuildingRegistry;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModHuts
@@ -16,15 +12,6 @@ public final class ModHuts
             DeferredRegister.create(
                     IBuildingRegistry.getInstance().key(),
                     AllTheColonists.MODID
-            );
-
-    public static final DeferredHolder<BuildingEntry, BuildingEntry> MEKANISM =
-            BUILDINGS.register(
-                    "mekanism",
-                    () -> new BuildingEntry.Builder()
-                            .setBuildingBlock(ModBlocks.MEKANISM_WORKER_HUT)
-                            .setBuildingClass(BuildingMekanism.class)
-                            .createBuildingEntry()
             );
 
     public static void register(final IEventBus bus)
