4eac83841327f6982e8a8044f7a29386cd447939
Cranky <marco_reko@hotmail.de>
2026-03-02 20:37:09 +0100

    Allow mekanist infuser recipes without mechanic output restrictions

--- src/main/java/com/allthecolonists/core/colony/buildings/BuildingMekanistHut.java
+++ src/main/java/com/allthecolonists/core/colony/buildings/BuildingMekanistHut.java
@@ -44,7 +44,9 @@ public class BuildingMekanistHut extends AbstractBuilding {
         @Override
         public boolean isRecipeCompatible(@NotNull final IGenericRecipe recipe) {
             final var intermediate = recipe.getIntermediate();
-            if (intermediate == null || intermediate == Blocks.AIR) return false;
+            if (intermediate == null || intermediate == Blocks.AIR) {
+                return false;
+            }
             return METALLURGIC_INFUSER_ID.equals(BuiltInRegistries.BLOCK.getKey(intermediate));
         }
 
